9ea9dbace06fde09b7eeb30ffecddbe85d26e980
dufontdd <dufont.dd@gmail.com>
2025-10-22 21:08:56 +0200

    task3

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void); /* <--- Ajoute cette ligne */
+void print_alphabet_x10(void);
+int _islower(int c);  /* <--- Ajoute cette ligne */
 
 #endif
