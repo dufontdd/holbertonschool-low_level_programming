0859367ba390097881c0ca92971d61ce9483f7d0
dufontdd <dufont.dd@gmail.com>
2025-10-22 13:35:50 +0200

    task4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in the lowercase,
+ * followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; [q-e!]*; ch++)
+	{
+		if (ch !='q' && ch != 'e')
+			putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
