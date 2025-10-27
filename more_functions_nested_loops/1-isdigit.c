7545a26acc14291f057c745f4282245de1ba2df8
dufontdd <dufont.dd@gmail.com>
2025-10-27 13:00:48 +0100

    task1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,15 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * _isdigit - affiche une suite de nbombre de 0 à 9
- * @ 0 - 9: La suite de nombre a afficher.
+ * _isdigit - Vérifie si un caractère est un chiffre (de 0 à 9)
+ * @c: Le caractère à vérifier
  *
- * Return: 1 si un chifre est affiché, 0 sinon.
+ * Return: 1 si c est un chiffre, 0 sinon
  */
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		return(1);
+		return (1);
 	else
-		return(0)
+		return (0);
 }
