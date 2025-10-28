9117ae7e168b46705c75f17e7254a0451d6e5c40
dufontdd <dufont.dd@gmail.com>
2025-10-28 09:40:33 +0100

    task0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * afficher une variable pointeur vers int d'une valeur 98
+ * @*p = variable utilisé pour cette tache
+ *
+ * Return Alwase 0.
+ */
+void reset_to_98(int *p)
+{
+	*p = 98;
+}
