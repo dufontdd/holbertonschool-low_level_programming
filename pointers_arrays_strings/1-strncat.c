22f0b9eb0bebfaeb09295f5129bcc7be7d385594
dufontdd <dufont.dd@gmail.com>
2025-10-30 22:39:37 +0100

    task0

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatène la chaine src à la fin de dest peut avoir,
+ * un caratère non nul si elle contien n octets
+ * @dest: destination de la chaine
+ * @src: source de la chaine
+ *
+ *  Return: pointeur dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	
+
+	/* trouver la fin de dest */
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	/* lie src a dest en ajoutant n a src */
+	for (j = 0; j < n && src[j] != = '\0'; j++,i++)
+		;
+	{
+		dest[i] = src[j];
+	}
+
+		/*finir dest avec dest */
+		dest[i] = '\0';
+
+		return (dest);
