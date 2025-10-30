f727c9ab994f1e7a5ec7eb01b119f68c0a090245
dufontdd <dufont.dd@gmail.com>
2025-10-30 23:10:50 +0100

    task1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,18 +11,16 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-	
 
 	/* trouver la fin de dest */
 	for (i = 0; dest[i] != '\0'; i++)
-	;
+		;
 
-	/* lie src a dest en ajoutant n a src */
-	for (j = 0; j < n && src[j] != '\0'; j++,i++)
-	dest[i] = src[j];
+	/* ajouter au maximum n caractères de src */
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
 
-
-	/*finir dest avec dest */
+	/* terminer dest avec un caractère nul */
 	dest[i] = '\0';
 
 	return (dest);
