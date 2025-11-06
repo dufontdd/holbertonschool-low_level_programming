6032e948d9bb01d7a2cb6a7e5390070146ed4f4a
dufontdd <dufont.dd@gmail.com>
2025-11-06 13:10:17 +0100

    afficher la valeur n de la factoria

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * factorial - calcule la factorielle d'un nombre
- * @n: entier utiliser pour calculer la factorielle
+ * factorial - calcule la factorielle d’un nombre
+ * @n: nombre dont on veut la factorielle
  *
  * Return: la factorielle de n, ou -1 si n < 0
  */
 int factorial(int n)
 {
 	if (n < 0)
-		return(-1);
-	if (n == 0)
-		return(1);
+		return (-1);
 
-	return (n * factoral(n - 1));
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
 }
