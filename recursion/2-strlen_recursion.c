b8aa30dc37d2ac67f482596b508c12627d2af90d
dufontdd <dufont.dd@gmail.com>
2025-11-06 12:33:40 +0100

    conaitre la longeur de la chaîne

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - permet de calculer la longeur d'une chaîne
- * @s: connaîtr la longeur de la chaîne
+ * _strlen_recursion - calcule la longueur d'une chaîne de caractères
+ * @s: chaîne dont on veut connaître la longueur
  *
- *  Return: longeur de la chaîne
+ * Return: longueur de la chaîne
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return(0);
+		return (0);
 
-	return( 1 + _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
