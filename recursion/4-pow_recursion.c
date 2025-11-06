8afbb2993527fe4a40014bda4127a2aea2b3cbed
dufontdd <dufont.dd@gmail.com>
2025-11-06 16:51:09 +0100

    retourne x à la puissance y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _pow_recursion - calcule x à la puissance y
+ * _pow_recursion - retourne x à la puissance y
  * @x: base
  * @y: exposant
  *
- * Return: pow 0 ou -1 si y < 0
+ * Return: x à la puissance y, ou -1 si y < 0
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return (-1;);
+		return (-1);
+
 	if (y == 0)
 		return (1);
+
 	return (x * _pow_recursion(x, y - 1));
-{
+}
