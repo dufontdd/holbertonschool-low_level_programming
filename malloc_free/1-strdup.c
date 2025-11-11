e259471a3bbfc842a195ab8a26df85db48532934
dufontdd <dufont.dd@gmail.com>
2025-11-11 23:42:40 +0100

    renvoyer le pointeur str ver buffer en utilisant malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -4,8 +4,29 @@
 /**
  * _strdup - renvoie un pointeur vers une nouvelle chaîne de caractères,
  *  copie de la chaîne `str`. La mémoire est allouée
- *  @str
+ *  @str chaîne source
+ *
+ *  Return: pointeurvers la nouvelle chaîne, ou NULL si erreur
  */
 char *_strdup(char *str)
 {
-	
+	char *buffer;
+	unsigned int i, size;
+
+	if (str == NULL)
+		return(NULL);
+
+	for (size = 0; str[size] != '\0'; size++)
+		;
+
+	buffer = malloc(sizeof(char) * (size + 1));
+	if (buffer == NULL)
+		return(NULL);
+
+	for (i = 0; i < size; i++)
+		buffer[i] = str[i];
+	buffer[i] = '\0';
+
+
+	return(buffer);
+}
