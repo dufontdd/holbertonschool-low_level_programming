b17e7a8fe364eca889f12bb46780d2df9d650061
dufontdd <dufont.dd@gmail.com>
2025-11-12 14:35:01 +0100

    corections betty code style task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,28 +1,24 @@
-#include "main.h"
-#include <stdlib.h>
-
 /**
- * create_array - creates an array of chars, and initializes it with a char
- * @size: size of the array to allocate
- * @c: char to initialize the array with
+ * create_array - Crée un tableau de caractères et l'initialise avec un char
+ * @size: taille du tableau à allouer
+ * @c: caractère d'initialisation
  *
- * Return: pointer to the array, or NULL if size = 0 or malloc fails
+ * Return: pointeur vers le tableau, ou NULL si size = 0 ou en cas d'échec
  */
 char *create_array(unsigned int size, char c)
 {
-	char *buffer;
 	unsigned int i;
+	char *buffer;
 
 	if (size == 0)
 		return (NULL);
 
-	buffer = malloc(sizeof(char) * (size + 1));
+	buffer = malloc(sizeof(char) * size);
 	if (buffer == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 		buffer[i] = c;
-	buffer[i] = '\0';
 
 	return (buffer);
 }
