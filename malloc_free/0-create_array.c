de8ca78101c944ed6c2505f1d1be94f5a331258d
dufontdd <dufont.dd@gmail.com>
2025-11-12 13:49:18 +0100

    corections betty code style task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,8 +15,8 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	buffer = malloc(sizeof(char) * size);
-		if (buffer == NULL)
-			return (NULL);
+	if (buffer == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		buffer[i] = c;	
