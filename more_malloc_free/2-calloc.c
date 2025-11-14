131824d7f37841fdcadf1bc5b1f222265e5096a5
dufontdd <dufont.dd@gmail.com>
2025-11-14 10:10:33 +0100

    alloue de la mémoire pour un tableau de  éléments de  octets chacun et renvoie un pointeur vers la mémoire allouée

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - ecrire une fonction qui allouer de la mémoire,
+ * pour un tableau, en utilisant `calloc`
+ *@nmemb: tableau a allouer dans le tableau
+ *@size: taille du tableau à allouer dans calloc
+ *
+ * Return: malloc ou NULL si malloc échoue
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *buff;
+	unsigned int total;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+
+	total = nmemb * size;
+
+	buff = malloc(total);
+	if (buff == 0)
+		return (NULL)
+
+	for (i = 0; i < total; i++)
+		buff[i] = 0;
+
+	return (buff)
+}
