3dfbec3746977b125c0d2af7f2ee8953436f1896
dufontdd <dufont.dd@gmail.com>
2025-11-19 18:29:22 +0100

    feat searches for an integer using a comparison function

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -10,7 +10,7 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i;
+	int i;
 
 	if (size <= 0)
 		return (-1);
@@ -20,7 +20,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-i
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(arry[i] != 0));
