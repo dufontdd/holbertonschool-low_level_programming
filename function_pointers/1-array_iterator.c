77c11228989def0b1fdc653ef01ad29433b50f24
dufontdd <dufont.dd@gmail.com>
2025-11-19 18:18:38 +0100

    feat 0-print_name.c: rappeler la fonction name; 1-array_iterator.c: use a fonction who execute une fonction in a arry

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -6,16 +6,14 @@
  * @size: is the size of the array
  * @array: is the chain wo i will execute all fonction
  * @acction: is a pointer to the function you need to use
- *
- * Return: void
  */
 void arry_interator(int *arry, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	size_t i;
 
 	if (arry == NULL || action == NULL)
 		return;
+
 	for (i = 0; i < size; i++)
 		action(arry[i]);
-return;	
 }
