923b28dfef22b324c0d1884e65d22e0c40a93bf4
dufontdd <dufont.dd@gmail.com>
2025-12-03 19:34:30 +0100

    add: directory doubly_linked_lists

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - ajoute un nouveau noeud au début d'une dlistint_t
+ * @head: adresse du pointeur vers le premier noeud
+ * @n: valeur du nouveau noeud
+ *
+ * Return: adresse du nouveau noeud, ou NULL si échec
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	/* si la liste n'est pas vide, mettre à jour prev du premier noeud */
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	/* la tête devient le nouveau noeud */
+	*head = new_node;
+
+	return (new_node);
+}
