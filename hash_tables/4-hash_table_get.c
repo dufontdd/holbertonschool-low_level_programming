b8ff1e0cc8b434384991d121c4bb8786e01a7ea5
dufontdd <dufont.dd@gmail.com>
2025-12-05 18:37:51 +0100

    add: file 4-hash_table_get.c, retrieves the value associated with a key(fix betty)

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,30 +1,28 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - récupère la valeur associée à une clé
- * @ht: la table de hachage
- * @key: la clé recherchée
+ * hash_table_get - retrieves the value associated with a key
+ * @ht: the hash table
+ * @key: the key they were looking for
  *
- * Return: la valeur associée, ou NULL si non trouvée
+ * Return: the associated value, or NULL if not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *node;
-
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
-
-    index = key_index((const unsigned char *)key, ht->size);
-    node = ht->array[index];
-
-    while (node != NULL)
-    {
-        if (strcmp(node->key, key) == 0)
-            return (node->value);
-
-        node = node->next;
-    }
-
-    return (NULL);
+	unsigned long int index;
+	hash_node_t *node;
+	
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	
+	return (NULL);
 }
