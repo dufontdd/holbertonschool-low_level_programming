77a1be8b34759c4be7373787b6f6f2c05415630a
dufontdd <dufont.dd@gmail.com>
2025-12-06 19:50:16 +0100

    add: file 4-hash_table_get.c,Retrieves a value associated with a key in a hash table

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,28 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - retrieves the value associated with a key
- * @ht: the hash table
- * @key: the key they were looking for
+ * hash_table_get - Retrieves a value associated with a key in a hash table
+ * @ht: The hash table to search
+ * @key: The key to look for
  *
- * Return: the associated value, or NULL if not found
+ * Return: The value associated with the key, or NULL if the key is not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-	hash_node_t *node;
-	
+	hash_node_t *current_node;
+
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
-	
-	while (node != NULL)
+	current_node = ht->array[index];
+
+	while (current_node != NULL)
 	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
-		node = node->next;
+		if (strcmp(current_node->key, key) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
 	}
-	
+
 	return (NULL);
 }
